fdedc5003a64dcbd9711b541450a4271ff779ff7
Joao Lucas <joaopedroramosdelucas10@gmail.com>
2023-06-05 16:46:38 -0300

    Adicionando VAR e CONVERSOES :sparkles:

---
+++ booleanos/boolean.js
@@ -0,0 +1,28 @@
+// BOOLEAN
+
+// const usuarioLogado = true;
+// const contaPaga = false;
+
+//truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+// console.log(0 == false)
+// console.log("" == false)
+// console.log(1 == true)
+
+
+
+// null (vazio ou nada) (null as vezes pode ter o typeof como Objeto)
+// undefinined
+
+let minhaVar;
+let verNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+
+console.log(typeof minhaVar)
+console.log(typeof verNull)
---
+++ conversao/cnversao.js
@@ -0,0 +1,18 @@
+// tipo de dado 
+// booleano
+
+// conversao implicita
+const numero = 456;
+const numeroStr = "456";
+
+// console.log(numero == numeroStr)
+
+// Number(transformar string em numero)
+// String(transformar numero em string)
+
+// conversao explicita
+console.log(numero + Number(numeroStr))
+ 
+
+
+
